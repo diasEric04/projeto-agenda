b42d12d80487673a7154e6fe728673e05780ff6b
Eric Arruda Dias <ericarrudadias@gmail.com>
2023-07-12 17:04:25 -0300

    criando partial para head, para header do site com input de search, paginação e a view de search.

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render  # get_object_or_404
+from django.shortcuts import render, redirect  # get_object_or_404
+from django.db.models import Q
 from contact.models import Contact
-from django.http import Http404
+from django.http import Http404, HttpRequest
+from django.core.paginator import Paginator
 
 
 def index(request):
@@ -8,11 +10,50 @@ def index(request):
         .filter(show=True)\
         .order_by('-id')\
 
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
         'title': 'Home',
-        'contacts': contacts
+        'page_obj': page_obj
     }
-    print(contacts)
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+
+def search(request: HttpRequest):
+    search_value = request.GET.get('q', '').strip()
+
+    if not search_value:
+        return redirect('contact:index')
+
+    # iexact = exatamente o que tem escrito, sem considerar letras maisculas
+    # exact = exatament o que tem escrito
+    # contains = contem um trecho no campo
+    # icontains = contem um techo no campo, sem considerar letras maisculas
+    contacts = Contact.objects\
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(email__icontains=search_value) |
+            Q(phone__icontains=search_value)
+        )\
+        .order_by('-id')\
+        # [10:20] = [limit:offset] na consulta
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'title': 'Home',
+        'page_obj': page_obj
+    }
     return render(
         request,
         'contact/index.html',
