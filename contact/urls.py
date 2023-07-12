dfe78cd2f71fbe7b08771fa2920dfed9ea002cad
Eric Arruda Dias <ericarrudadias@gmail.com>
2023-07-12 12:17:00 -0300

    pagina de contato unico, pagina de todos os contatos e header do site

--- contact/urls.py
+++ contact/urls.py
@@ -7,7 +7,8 @@ from . import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('<int:contact_id>/', views.contact, name='contact'),
+    path('', views.index, name='index'),
 ]
 
 
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,12 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render  # get_object_or_404
+from contact.models import Contact
+from django.http import Http404
 
 
 def index(request):
+    contacts = Contact.objects\
+        .filter(show=True)\
+        .order_by('-id')\
+
     context = {
-        'title': 'Home'
+        'title': 'Home',
+        'contacts': contacts
     }
+    print(contacts)
     return render(
         request,
         'contact/index.html',
+        context
+    )
+
+
+def contact(request, contact_id):
+
+    single_contact = Contact.objects.filter(id=contact_id, show=True).first()
+
+    # single_contact = get_object_or_404(
+    #   Contact.objects.filter(id=contact_id).first()
+    # )
+
+    # metodo last retorna o ultimo elemento da queryset
+    # metodo first retorna primeiro elemento da queryset
+
+    if single_contact is None:
+        raise Http404()
+
+    page_title = f'{single_contact.first_name} {single_contact.last_name}'
+    context = {
+        'title': page_title or 'not found',
+        'contact': single_contact
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
         context
     )
